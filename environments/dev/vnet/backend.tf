051057731d0483470590231a3a428e37507d48b4
karthikprasath2027 <karthikprasath2027@outlook.com>
2025-06-23 14:38:35 +0530

    only terraform

--- environments/dev/vnet/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "azure-devops"
-    storage_account_name  = "backendazure00"
-    container_name        = "tfstate"
-    key                   = "dev/vnet/terraform.tfstate"
-  }
-}
